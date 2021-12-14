b2dcbbe4a32e42b81039e334ea309e9e7a486c07
asyncfinkd <shamgoingzuck@gmail.com>
2021-12-14 20:07:50 +0400

    render by dynamically button text and change msg

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return { msg: 'hi' };
+    return { msg: 'Hello, This is test Message', btnText: 'Click me' };
   }
 }
